a2ba608cae7c092bc310cb28e176dff3426a562d
冯国富 <fgz2016@outlook.com>
2017-06-28 23:54:30 +0800

    新闻列表显示多种类型的item

--- app/src/main/java/example/newsclient/adapter/NewsAdapter.java
+++ app/src/main/java/example/newsclient/adapter/NewsAdapter.java
@@ -50,37 +50,110 @@ public class NewsAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //列表项数据
-      NewsEntity.ResultBean resultBeanInfo=mResultBeanList.get(position);
-       View view;
-        ViewHolder viewHolder;
-        if (convertView==null){
-            view = LayoutInflater.from(mContext).inflate(
-                    R.layout.news_item1, parent,false);
+        //获取item的类型
+        int type=getItemViewType(position);
+        if (type==TYPE_1){
+            //一张图片的类型
+            //列表项数据
+            NewsEntity.ResultBean resultBeanInfo=mResultBeanList.get(position);
+            View view;
+            ViewHolder viewHolder;
+            if (convertView==null){
+                view = LayoutInflater.from(mContext).inflate(
+                        R.layout.news_item1, parent,false);
 
-            viewHolder=new ViewHolder();
-            //绑定控件
-            viewHolder.ivIcon= (ImageView) view.findViewById(R.id.iv_icon);
-            viewHolder.tvTitle= (TextView) view.findViewById(R.id.tv_title);
-            viewHolder.tvSource= (TextView) view.findViewById(R.id.tv_source);
-            viewHolder.tvComment= (TextView) view.findViewById(R.id.tv_comment);
-            //将viewHolder存储在view中
-            view.setTag(viewHolder);
+                viewHolder=new ViewHolder();
+                //绑定控件
+                viewHolder.ivIcon= (ImageView) view.findViewById(R.id.iv_icon);
+                viewHolder.tvTitle= (TextView) view.findViewById(R.id.tv_title);
+                viewHolder.tvSource= (TextView) view.findViewById(R.id.tv_source);
+                viewHolder.tvComment= (TextView) view.findViewById(R.id.tv_comment);
+                //将viewHolder存储在view中
+                view.setTag(viewHolder);
+
+            }else {
+                view=convertView;
+                viewHolder= (ViewHolder) view.getTag();
+            }
+
+            // 4. 显示列表项中的子控件
+            viewHolder.tvTitle.setText(resultBeanInfo.getTitle());           // 显示标题
+            viewHolder.tvSource.setText(resultBeanInfo.getSource());           // 新闻来源
+            viewHolder.tvComment.setText(resultBeanInfo.getReplyCount() + "跟帖"); // 新闻来源
+            // 显示新闻图片
+            Picasso.with(mContext).load(resultBeanInfo.getImgsrc()).into(viewHolder.ivIcon);
+
+            return view;
 
         }else {
-            view=convertView;
-            viewHolder= (ViewHolder) view.getTag();
+            //三张图片的类型
+            NewsEntity.ResultBean resultBeanInfo=mResultBeanList.get(position);
+            View view;
+            ViewHolder2 viewHolder;
+            if (convertView==null){
+                view = LayoutInflater.from(mContext).inflate(
+                        R.layout.news_item2, parent,false);
+
+                viewHolder=new ViewHolder2();
+
+                //绑定控件
+                viewHolder.mTextView_title= (TextView) view.findViewById(R.id.tv_news_title);
+                viewHolder.mTextView_Comment= (TextView) view.findViewById(R.id.tv_comment_count);
+                viewHolder.mImageView_one= (ImageView) view.findViewById(R.id.iv_01);
+                viewHolder.mImageView_two= (ImageView) view.findViewById(R.id.iv_02);
+                viewHolder.mImageView_three= (ImageView) view.findViewById(R.id.iv_03);
+                //将viewHolder存储在view中
+                view.setTag(viewHolder);
+
+            }else {
+                view=convertView;
+                viewHolder= (ViewHolder2) view.getTag();
+            }
+
+            // 4. 显示列表项中的子控件
+            viewHolder.mTextView_title.setText(resultBeanInfo.getTitle());           // 显示标题
+            viewHolder.mTextView_Comment.setText(resultBeanInfo.getReplyCount() + "跟帖"); // 新闻来源
+            // 显示3张新闻图片
+            Picasso.with(mContext).load(resultBeanInfo.getImgsrc()).into(viewHolder.mImageView_one);
+            Picasso.with(mContext).load(resultBeanInfo.getImgextra().get(0).getImgsrc()).into(viewHolder.mImageView_two);
+            Picasso.with(mContext).load(resultBeanInfo.getImgextra().get(1).getImgsrc()).into(viewHolder.mImageView_three);
+
+            return view;
+
         }
 
-        // 4. 显示列表项中的子控件
-        viewHolder.tvTitle.setText(resultBeanInfo.getTitle());           // 显示标题
-        viewHolder.tvSource.setText(resultBeanInfo.getSource());           // 新闻来源
-        viewHolder.tvComment.setText(resultBeanInfo.getReplyCount() + "跟帖"); // 新闻来源
-        // 显示新闻图片
-       Picasso.with(mContext).load(resultBeanInfo.getImgsrc()).into(viewHolder.ivIcon);
-
-        return view;
     }
+
+    /* ---------------列表显示多种类型 -------------------*/
+    private static final int TYPE_1=0;//一张图片的类型
+    private static final int TYPE_3=1;//三张图片的类型
+
+    /**
+     *返回列表项属于哪一种类型的item
+     * */
+    @Override
+    public int getItemViewType(int position) {
+        //列表项对应的实体数据
+        NewsEntity.ResultBean news= (NewsEntity.ResultBean) getItem(position);
+        //判断是哪一种类型
+        if (news.getImgextra()!=null&&news.getImgextra().size()>0){
+            //三张图片的类型
+            return TYPE_3;
+        }else {
+            return TYPE_1;
+        }
+
+    }
+
+    /**
+     *返回有多少种不同类型的列表项
+     * */
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+     /*------- 列表显示多种类型 -----------------*/
 
     class ViewHolder{
 
@@ -88,5 +161,14 @@ public class NewsAdapter extends BaseAdapter{
         TextView tvTitle;
         TextView tvSource;
         TextView tvComment;
+
+    }
+
+    class ViewHolder2{
+        TextView mTextView_title;
+        TextView mTextView_Comment;
+        ImageView mImageView_one;
+        ImageView mImageView_two;
+        ImageView mImageView_three;
     }
 }
